09194fcd1f540a68647ce3f82aeb57d8245fa92c
Hossein <Hossein-cf>
2023-03-19 00:46:44 +0330

    Initial commit

---
+++ Gudulion.BackEnd/Gudulion.BackEnd/Moduls/User/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace Gudulion.BackEnd.Moduls.User;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime BirthDate { get; set; }
+    public Gender Gender { get; set; }
+}
+
+public enum Gender
+{
+    Male,
+    Female
+}
