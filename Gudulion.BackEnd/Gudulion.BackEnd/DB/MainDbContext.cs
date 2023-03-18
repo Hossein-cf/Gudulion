5930a6de1597b28440694036a0b5e80725f186af
Hossein <Hossein-cf>
2023-03-19 01:31:38 +0330

    add db context to project

---
+++ Gudulion.BackEnd/Gudulion.BackEnd/DB/MainDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Gudulion.BackEnd.Moduls.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gudulion.BackEnd.DB;
+
+public class MainDbContext:DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public MainDbContext()
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Gudulion;Integrated Security=True");
+
+    }
+}
